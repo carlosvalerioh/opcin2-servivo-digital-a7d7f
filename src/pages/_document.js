0d8ce7b3162aa11a766100cd328868c9a1b54083
Stackbit Code Editor <projects@stackbit.com>
2021-06-21 17:15:50 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -52,15 +52,15 @@ export default class MyDocument extends Document {
                         }}
                 />
                 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
 
-  gtag('config', 'UA-199953511-1');
-  gtag('config', 'AW-347510295');
-  gtag('config', 'AW-347510295/3BVCCKUFEJes2qUB', {
-    'phone_conversion_number': '5552134270'
-  });
+                    gtag('config', 'UA-199953511-1');
+                    gtag('config', 'AW-347510295');
+                    gtag('config', 'AW-347510295/3BVCCKUFEJes2qUB', {
+                        'phone_conversion_number': '5552134270'
+                    });
 </script>
                 </Head>
                 <body {...this.helmetBodyAttrComponents}>
