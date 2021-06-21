e5e9992eb63e5c6a1c6a4a40fbff3d407f9511ad
Stackbit Code Editor <projects@stackbit.com>
2021-06-21 17:17:59 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -55,10 +55,6 @@ export default class MyDocument extends Document {
                         });
                     `,
                         }}
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-
                         
                 />
                 </Head>
