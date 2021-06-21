3ce4e16fd60f8665efb6bb7615bb02a924c9bb74
Stackbit Code Editor <projects@stackbit.com>
2021-06-21 17:17:45 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -54,11 +54,7 @@ export default class MyDocument extends Document {
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
 
-                        gtag('config', 'UA-199953511-1');
-                        gtag('config', 'AW-347510295');
-                        gtag('config', 'AW-347510295/3BVCCKUFEJes2qUB', {
-                            'phone_conversion_number': '5552134270'
-                        });
+                        
                 />
                 </Head>
                 <body {...this.helmetBodyAttrComponents}>
