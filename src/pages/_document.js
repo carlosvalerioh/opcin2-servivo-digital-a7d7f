3c5209b138808eafae326f0f5315878698258137
Stackbit Code Editor <projects@stackbit.com>
2021-06-21 17:17:50 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -45,6 +45,11 @@ export default class MyDocument extends Document {
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
+                        gtag('config', 'UA-199953511-1');
+                        gtag('config', 'AW-347510295');
+                        gtag('config', 'AW-347510295/3BVCCKUFEJes2qUB', {
+                            'phone_conversion_number': '5552134270'
+                        });
                         gtag('config', '${GA_TRACKING_ID}', {
                         page_path: window.location.pathname,
                         });
